9e5e90446e2074f1ba25c0c26e7bd9e1577121bb
mubashirshayan <121729482+mubashirshayan@users.noreply.github.com>
2023-04-19 20:42:36 +0500

    New files

---
+++ app.js
@@ -0,0 +1,31 @@
+//JavaScript Assignment no2 Chapter 4:
+//Question no1
+
+/*window.alert('Alert!')
+var variable1; var variable2; var variable*/
+//Question no2
+//'Legal variable name'
+/*var camelCase;
+var _name;
+var name2;
+var camel_case;
+var name;*/
+//Illegal variable name
+/*var 2name
+var for;
+var Store;
+var variable name;
+var !name;
+*/
+//Question no3
+/*var str='Rules for naming JS variables'
+var firstA='Variable names can only contain name,number,or $and_.'
+var secB='Variables must begin with a name,$name or camelCase. For example $name, _name or name'
+var thdC='Variable names are case Sensetive.'
+var forthD='Variable names should not be JS Keyword.'
+document.write('<h1>Rules for naming JS variables</h1>');
+document.write(firstA+'<br>');
+document.write(secB+'<br>');
+document.write(thdC+'<br>');
+document.write(forthD+'<br>');
+*/
